7184c9f29206a517c2ee26874417ab1ddfd110a3
abhinavavhad <abhinavavhad2002@gmail.com>
2025-01-31 00:51:32 +0530

    commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
     {songName: "Warriyo Mortals", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Cielo-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
     {songName: "DEAF Invincible", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "Heaven - Heart", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
